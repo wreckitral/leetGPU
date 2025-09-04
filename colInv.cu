2df64570c0456f88bc8202b10999e3c4d2db8352
wreckitral <defhanayasofhiea@gmail.com>
2025-09-04 12:39:20 +0700

    feat: add color inversion answer

--- colInv.cu
+++ colInv.cu
@@ -1,3 +1,10 @@
 __global__ void invert_kernel(unsigned char* image, int width, int height) {
-    int i
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if(x < width * height) {
+        int startIdx = x * 4;
+        image[startIdx] = 255 - image[startIdx];
+        image[startIdx + 1] = 255 - image[startIdx + 1];
+        image[startIdx + 2] = 255 - image[startIdx + 2];
+    }
 }
